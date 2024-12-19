97cefd6b1107e0ef3133699967dc917b8b141c7d
ghostdp123 <ghostdp123@gmail.com>
2024-12-19 11:56:23 +0800

    测试neon

---
+++ src/app/api/comments/route.ts
@@ -0,0 +1,56 @@
+
+import { neon } from '@neondatabase/serverless'
+import { NextResponse } from 'next/server'
+
+const sql = neon(`${process.env.DATABASE_URL}`)
+
+export async function GET() {
+  const result = await sql`SELECT * FROM comments`
+  return NextResponse.json({
+    code: 0,
+    data: result
+  })
+}
+
+export async function POST(request: Request) {
+  const { comment } = await request.json()
+  const result = await sql('INSERT INTO comments (comment) VALUES ($1)', [comment])
+  if (result) {
+    return NextResponse.json({
+      code: 0
+    })
+  } else {
+    return NextResponse.json({
+      code: -1
+    })
+  }
+}
+
+export async function PUT(request: Request) {
+  const { comment } = await request.json()
+  const result = await sql('UPDATE comments SET comment = $1 WHERE id = 3', [comment])
+  if (result) {
+    return NextResponse.json({
+      code: 0
+    })
+  } else {
+    return NextResponse.json({
+      code: -1
+    })
+  }
+}
+
+export async function DELETE(request: Request) {
+  const searchParams = new URL(request.url).searchParams
+  const id = searchParams.get('id')
+  const result = await sql('DELETE FROM comments WHERE id = $1', [id])
+  if (result) {
+    return NextResponse.json({
+      code: 0
+    })
+  } else {
+    return NextResponse.json({
+      code: -1
+    })
+  }
+}
