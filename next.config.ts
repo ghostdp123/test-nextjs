5ba465eae4e0ac4c9481fdf2a814d74f32f53ca3
ghostdp123 <ghostdp123@gmail.com>
2024-12-22 20:02:46 +0800

    bugfix

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'duyi-resource.oss-cn-beijing.aliyuncs.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.nlark.com'
       }
     ],
   }
