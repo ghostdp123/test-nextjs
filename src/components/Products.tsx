1fc8b71c2e736d640d418dd3b8378c49d667f2cf
ghostdp123 <ghostdp123@gmail.com>
2024-12-22 16:59:13 +0800

    bugfix

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,14 +1,14 @@
 
-// import { Product } from "@/types/global"
-// import Image from "next/image";
+import { Product } from "@/types/global"
+import Image from "next/image";
 
 export default async function Products() {
-  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`)
-  // const products: { code: number; data: Product[] } = await res.json()
+  const res = await fetch(`${process.env.NEXT_BASE_URL}/api/products`)
+  const products: { code: number; data: Product[] } = await res.json()
   return (
     <div className="flex-1">
       <h1 className="mb-8 text-4xl">All products</h1>
-      {/* <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-4">
         {products.data.map((product) => (
           <div key={product.id} className="bg-slate-50 p-4 rounded-lg shadow-md hover:bg-slate-200 transition duration-300 ease-in-out cursor-pointer">
             <Image src={product.image} alt={product.name} width={300} height={300} />
@@ -18,7 +18,7 @@ export default async function Products() {
             </div>
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   )
 }
