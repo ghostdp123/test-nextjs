c65565d2e3a6e9c19341398a48fa106dcce178de
ghostdp123 <ghostdp123@gmail.com>
2024-12-23 11:17:16 +0800

    fixbug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { rule } from "postcss"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,7 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  rule("no-unused-vars", "off"),
+  {
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
