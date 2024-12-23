e3544cb62d42a5c52b36665229f800d16dbf5a35
ghostdp123 <ghostdp123@gmail.com>
2024-12-23 11:30:27 +0800

    fixhug

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,7 +1,7 @@
 
 import { Product } from "@/types/global"
-// import Image from "next/image"
-// import { AddCart } from "@/components"
+import Image from "next/image"
+import { AddCart } from "@/components"
 
 export const dynamicParams = false
 
@@ -14,13 +14,13 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  // const { id } = await params
-  // const response = await fetch(`${process.env.NEXT_BASE_URL}/api/products/${id}`)
-  // const result: { code: number; data: Product } = await response.json()
-  // const product = result.data
+  const { id } = await params
+  const response = await fetch(`${process.env.NEXT_BASE_URL}/api/products/${id}`)
+  const result: { code: number; data: Product } = await response.json()
+  const product = result.data
   return (
     <div className="container flex py-6">
-      {/* <div className="w-64">
+      <div className="w-64">
         <h2 className="font-sans text-3xl leading-10 font-bold my-8">{product.name}</h2>
         <p className="leading-10">{product.description}</p>
       </div>
@@ -29,7 +29,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
           objectFit: 'cover',
         }} />
       </div>
-      <AddCart product={product} /> */}
+      <AddCart product={product} />
     </div>
   );
 }
